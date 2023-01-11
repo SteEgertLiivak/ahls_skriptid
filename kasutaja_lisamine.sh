5d2aca84fbbdd5daa6318e7758451e7322c2d90d
Sten Egert Liivak <sten-egert.liivak@voco.ee>
2023-01-11 09:56:42 +0200

    värskendus

--- kasutaja_lisamine.sh
+++ kasutaja_lisamine.sh
@@ -2,7 +2,23 @@
 #kasutaja lisamine
 #juhend käsu täitmiseks 
  
-echo "Sisestage uue kasutaja nimi: $nimi "
-read nimi
+#echo "Sisestage uue kasutaja nimi: $nimi
+#read nimi
 
-useradd $nimi
+#useradd $nimi
+
+#teine lahendus
+if [ $# -ne 1 ]; then
+	echo "Kasutusjuhend: $0 kasutajanimi"
+else
+	kasutajanimi=$1
+	useradd $kasutajanimi -m -s /bin/bash
+	if [ $? -eq 0 ]; then 
+		echo "Kasutaja nimega $kasutajanimi on lisatud süsteemi"
+		cat /etc/passwd | grep $kasutajanimi
+		ls -la /home/$kasutajanimi
+	else
+		echo "probleem kasutaja $kasutajanimi lisamisega"
+		echo "probleemkood on $?"
+	fi
+fi
